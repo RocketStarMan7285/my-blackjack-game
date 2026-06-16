9654d6ca1884c3635705a6305480177803dca3fb
NoahD <nd1493914@gmail.com>
2026-06-15 21:20:12 -0400

    refined documentation of classes in CardClass.py

--- CardClass.py
+++ CardClass.py
@@ -1,7 +1,10 @@
+# Class Definitions 
 import random
 
-#key: card rank, value: point value based on rank
-rank_ptval = { #dictionary
+# Dictionary: rank_ptval
+# Key: the rank of the card (string) 
+# Value: point value based on rank (integer)
+rank_ptval = { 
                 "2": 2,
                 "3": 3,
                 "4": 4,
@@ -11,41 +14,47 @@ rank_ptval = { #dictionary
                 "8": 8,
                 "9": 9,
                 "10": 10,
+                # -Picture cards here are worth 10 points each- #
                 "Jack": 10,
                 "Queen": 10,
                 "King": 10,
-                "Ace": 11 #an ace can also be a one
+                # --------------------------------------------- #
+                # An ace can also be worth one point depending on the hand, logic needed #
+                "Ace": 11 
                 }
 
-### Class that contains data for a single playing card ###
-# members:
-# -string suit: Hearts, Clubs, Spades, Diamonds
-# -string rank: 
-#   -2 to 10 
-#   -10-point cards: Jack, Queen, King 
-#   -Ace: either one point or 11 points depending on hand
-### -------------------------------------------------- ###    
+### ----------------------Card Class---------------------- ###
 class Card:
+    '''
+    Class that contains data for a single playing card
 
-    #initializer
+    Members:
+        - suit (string): Hearts, Clubs, Spades, Diamonds
+        - rank (string): represents the point value of the card as a string 
+        - pt_val (integer): the point value of the card, determined by the rank 
+    '''
+
+    # Initializer
     def __init__(self, suit, rank):
         self.suit = suit #string
         self.rank = rank #string
         self.pt_val = rank_ptval[rank] #the point value of each card is the key 
 
     ### METHODS TO TEST OUT CARD OBJECTS ###
-    #print the suit and rank of the card
+    # Print the suit and rank of the card
     def __str__(self):
         return f"{self.rank} of {self.suit}"
     
-    #get the point value of each card
+    # Get the point value of each card
     def get_ptval(self):
          return self.pt_val
+### ------------------------------------------------------ ###
 
-### class that contains a list of 52 unique playing cards in a deck ###
+### ----------------------Deck Class---------------------- ###
 class Deck:
+    '''Class that contains a list of 52 unique playing cards in a deck '''
 
-    #initializer: create a deck of 52 unique cards
+    # Initializer: create a deck of 52 unique cards
     def __init__(self):
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         ranks = list(rank_ptval.keys()) #return a list of the keys in the rank_ptval dictionary
@@ -55,32 +64,33 @@ class Deck:
         print("Shuffling deck...")
         random.shuffle(self.cards)
 
-    #print the amount of cards that are left in the deck
+    # Print the amount of cards that are left in the deck
     def print_deck_amt(self):
         print(f"The deck has {len(self.cards)} cards")
 
-    #deal a set number of cards to a player's hand (defaulted to 1)
+    # Deal a set number of cards to a player's hand (defaulted to 1)
     def deal_card(self, player, num_cards = 1, printCards = False):
         
-        #before executing anything, throw an error if 
-        #the amount of cards left in the deck is less than num_cards
+        # before executing anything, throw an error if 
+        # the amount of cards left in the deck is less than num_cards
         if(len(self.cards) < num_cards): 
             raise ValueError("Can't deal anymore cards")
         
-        #retrieve the first (num_cards) from the deck and remove
+        # retrieve the first (num_cards) from the deck and remove
         cards_to_deal = self.cards[:num_cards]
         if printCards == True:
             print("Dealt: ")
             for i in cards_to_deal:
                 print(i, end=" ")
-        #update the original list of cards to exclude the dealt cards
+        # update the original list of cards to exclude the dealt cards
         self.cards = self.cards[num_cards:]
-        #add the cards to the player's hand
-        player.add_to_hand(cards_to_deal)
-
-        
-### class that contains a list of all the cards in the player's hand ###
+        # add the cards to the player's hand
+        player.add_to_hand(cards_to_deal)       
+### ------------------------------------------------------ ###
+       
+### ----------------------Player Class---------------------- ###
 class Player:
+    '''Class that contains a list of all the cards in the player's hand'''
 
     def __init__(self):
         self.card_list = [] #list of cards in a player's hand
@@ -109,6 +119,5 @@ class Player:
         if isinstance(cards, list):
             self.card_list.extend(cards) #deal multiple cards
         else:
-            self.hand.appead(cards) #deal a single card
-        
-
+            self.card_list.append(cards) #deal a single card
+### ------------------------------------------------------ ### 
